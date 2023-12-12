88bfc825c296d071e5100677a8c261e3e25b14b3
amalmechergui <amal123mechergui@gmail.com>
2023-12-12 19:54:16 +0100

    CKP_NodeJS

---
+++ read-file.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+
+fs.writeFile('welcome.txt', 'Hello Node', (err) => {
+  if (err) throw err;
+  console.log('File created!');
+});
+
+
+fs.readFile('hello.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log('Data from hello.txt:', data);
+});
